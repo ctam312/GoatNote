0852ffa0b368182b12621bedde4d4012f4fd984c
Ctam312 <ctam312@gmail.com>
2023-02-21 18:35:58 -0500

    db models finished

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db, environment, SCHEMA
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
